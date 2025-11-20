44acc7c43b05e28c6a0b0d2fb6572af42c0f1498
Avneet Singh Sidhu <avneetsidhu18@gmail.com>
2025-11-20 17:59:55 +0530

    image uploader

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
 import Event from "@/database/event.model";
+import { v2 as cloudinary } from "cloudinary";
 
 export async function POST(req:NextRequest){
     try{
@@ -14,6 +15,21 @@ export async function POST(req:NextRequest){
             return NextResponse.json({message:error},{status:400})
         }
 
+        const file=formdata.get("image") as File;
+        if(!file)
+            return NextResponse.json({message:"Image is required"},{status:400})
+        const arraybuffer= await file.arrayBuffer();
+        const buffer=Buffer.from(arraybuffer);
+        
+        const uploadres=await new Promise((resolve,reject)=>{
+            cloudinary.uploader.upload_stream({resource_type:"image",folder:"events"},(error,result)=>{
+                if(error) reject(error);
+
+                resolve(result);
+            }).end(buffer);
+        })
+        event.image=(uploadres as {secure_url:string}).secure_url;
+        
         const createdEvent= await Event.create(event);
         return NextResponse.json({message:"Event created successfully",event:createdEvent},{status:201})
     } catch(error){
